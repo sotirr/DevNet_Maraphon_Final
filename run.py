4f4619092ad0bec8700c3b683dff537649b3c86b
sotirr <47517203+sotirr@users.noreply.github.com>
2020-05-06 12:06:02 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ from get_topology import get_topology
 from draw_topology import draw_topology
 
 
-topology_file = 'topology.yaml'
-get_topology(topology_file)
-draw_topology(topology_file)
+get_topology()
+draw_topology()
